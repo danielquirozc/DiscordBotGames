a72db181b7a185f9f16fe25c23724fac87d9c942
AmChiwa <142172231+danielquirozc@users.noreply.github.com>
2024-06-09 12:36:48 -0400

    Add Clear Chat command

---
+++ commands/clear.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("clear")
+    .setDescription("Clear chat")
+    .addIntegerOption((option) =>
+      option
+        .setName("amount")
+        .setDescription("La cantidad de mensajes a borrar")
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    const amount = interaction.options.getInteger("amount");
+    if (amount <= 0) {
+      await interaction.reply("La cantidad debe ser mayor a 0");
+      return;
+    }
+    if (amount > 100) {
+      await interaction.reply("La cantidad debe ser menor o igual a 100");
+      return;
+    }
+    await interaction.channel.bulkDelete(amount);
+    await interaction.reply(`Borrado ${amount} mensajes`);
+  },
+};
