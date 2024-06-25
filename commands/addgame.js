1fc87c66f6e3c3a5877431d7940b6ed180f5d597
Daniel <142172231+danielquirozc@users.noreply.github.com>
2024-06-25 15:25:27 -0400

    Command added specifically to insert data to the database

--- commands/addgame.js
+++ commands/addgame.js
@@ -1,10 +1,10 @@
-const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require("discord.js");
+const { SlashCommandBuilder } = require("discord.js");
 const { convertToObjectArray } = require("../utils/convertToObjectArray");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("newgame")
-    .setDescription("Anuncia un nuevo juego")
+    .setName("addgame")
+    .setDescription("Agrega un juego a la base de datos")
     .addStringOption((option) =>
       option
         .setName("title")
@@ -20,13 +20,13 @@ module.exports = {
     .addStringOption((option) =>
       option
         .setName("size")
-        .setDescription("El tamaño del juego")
+        .setDescription("El tamaño del juego ejemplo 101MB, 1GB, 10GB")
         .setRequired(true)
     )
     .addStringOption((option) =>
       option
         .setName("image")
-        .setDescription("La URL de la imagen del juego")
+        .setDescription("Las URLs de la imagenes separada por comas")
         .setRequired(true)
     )
     .addStringOption((option) =>
@@ -34,9 +34,19 @@ module.exports = {
         .setName("botones")
         .setDescription("Ingresa los botones en formato nombre>valor, url>valor")
         .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("tipo")
+        .setDescription("Ingresa el tipo de juego")
+        .setRequired(true)
+        .addChoices(
+          { name: "Normal", value: "normal" },
+          { name: "+18", value: "+18" }
+        )
     ),
 
-  async execute(interaction) {
+  async execute(interaction, connection) {
     if (!interaction.member.permissions.has("ADMINISTRATOR")) {
       await interaction.reply({ content: "No tienes permisos suficientes para ejecutar este comando.", ephemeral: true });
       return;
@@ -46,46 +56,31 @@ module.exports = {
     const size = interaction.options.getString("size");
     const image = interaction.options.getString("image");
     const buttonsInFormat = interaction.options.getString("botones");
+    const tipo = interaction.options.getString("tipo");
+    let imgs;
     let downloadLinks;
+
     try {
+      imgs = image.split(",");
       downloadLinks = convertToObjectArray(buttonsInFormat);
-      if (
-        !Array.isArray(downloadLinks) ||
-        downloadLinks.some((link) => !link.nombre || !link.url)
-      ) {
+      if (!Array.isArray(downloadLinks) || downloadLinks.some((link) => !link.nombre || !link.url)) {
         throw new Error("Invalid format");
       }
     } catch (error) {
-      await interaction.reply({
-        content:
-          'Formato de botones inválido. Asegúrate de que esté en formato JSON y que cada botón tenga un "nombre" y un "url".',
-        ephemeral: true,
-      });
-      return;
-    }
-    const embed = new EmbedBuilder()
-      .setTitle(`${title} - ${size}`)
-      .setDescription(description)
-      .setColor(0x00ae86);
-
-    if (image) {
-      embed.setImage(image);
+     return await interaction.reply({ content: 'Formato de botones o imagenes inválido. Asegúrate de escribir bien el formato.', ephemeral: true });
     }
 
     // Crear los botones en filas (Discord permite hasta 5 botones por fila)
-    const rows = [];
-    for (let i = 0; i < downloadLinks.length; i += 5) {
-      const buttons = new ActionRowBuilder();
-      downloadLinks.slice(i, i + 5).forEach((link) => {
-        buttons.addComponents(
-          new ButtonBuilder()
-            .setLabel(link.nombre)
-            .setStyle(ButtonStyle.Link)
-            .setURL(link.url)
-        );
+    connection.query(`INSERT INTO games (title, description, size, images, links, type) VALUES (?, ?, ?, ?, ?, ?);`,
+      [title, description, size, JSON.stringify(imgs), JSON.stringify(downloadLinks), tipo],
+      async (err, result) => {
+        if (err) {
+          console.log(err);
+          await interaction.reply({ content: "Error al insertar el juego en la base de datos.", ephemeral: true });
+          return;
+        };
+        await interaction.reply({ content: "Juego insertado correctamente.", ephemeral: true });
+        return;
       });
-      rows.push(buttons);
-    }
-    await interaction.reply({ embeds: [embed], components: rows });
   },
 };
